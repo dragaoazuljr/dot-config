a407ebd8d28436cc824f8c34bd527d5eb5ffe738
Danillo Moraes <danillo.moraes@outlook.com>
2022-09-17 15:12:05 -0300

    refactor: nvim dependencies script

--- nvim/dependencies.md
+++
@@ -1,4 +0,0 @@
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-npm install -g typescript-language-server typescript
-npm install -g tree-sitter-cli
-npm install -g @fsouza/prettierd
--- nvim/install-dependencies.sh
+++ nvim/install-dependencies.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-filename='dependencies.md'
-n=1
-while read line; do
-  # reading each line
-  command $line
-  n=$((n+1))
-done < $filename
+source /etc/os-release
+
+DISTRO=$PRETTY_NAME
+
+if [[ $DISTRO == *"Fedora"* ]]; then
+  sudo dnf install libstdc++-static
+fi
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+npm install -g typescript-language-server typescript tree-sitter-cli @fsouza/prettierd
