52458924964077b9f618338ef3e211e15ce9213b
Danillo Moraes <danillo.moraes@outlook.com>
2022-10-13 14:30:12 -0300

    feat: added tab and shift-tab keybindings to cmp-nvim

--- nvim/after/plugin/cpm.rc.lua
+++ nvim/after/plugin/cpm.rc.lua
@@ -18,6 +18,20 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true
     }),
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else 
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else 
+        fallback()
+      end
+    end
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
